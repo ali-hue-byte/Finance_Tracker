f796e0dea1b0f82501c8df4b33207ceb3af075cd
ali-hue-byte <elguelouiali@gmail.com>
2026-05-31 13:09:46 +0200

    Refactor main function for improved readability

--- Finance Tracker CLI/finance_tracker/main.py
+++ Finance Tracker CLI/finance_tracker/main.py
@@ -1,3 +1,4 @@
+# CLI functions
 from .menu_driven_cli import (add_expense,
                   add_income,
                   list_transactions_cli,
@@ -15,6 +16,7 @@ def main():
     print("default expense categories: food, rent, transport, utilities, entertainment, health, miscellaneous")
     print()
     while True:
+        # Main menu
         print("1. Add Expense")
         print("2. Add Income")
         print("3. List Transactions")
@@ -27,61 +29,73 @@ def main():
         print("0. Exit")
 
         choice = input("Choose an option: ").strip()
+        # Add Expense
         if choice == "1":
             amount = input("Amount: ")
             category = input("Category: ")
             note = input("Note (optional): ")
             print(add_expense(amount, category, note))
             print()
+        # Add income
         elif choice == "2":
             amount = input("Amount: ")
             category = input("Category: ")
             note = input("Note (optional): ")
             print(add_income(amount, category, note))
             print()
+        # List Transactions
         elif choice == "3":
             category = input("Category (optional): ")
             ttypt = input("Transaction Type (optional): ")
             list_transactions_cli(category, ttypt)
             print()
+        # Show Balance
         elif choice == "4":
             print(show_balance())
             print()
+        # Show Categories
         elif choice == "5":
             print(show_categories())
             print()
+        # Show Types
         elif choice == "6":
             print(show_types())
             print()
+        # Total by Category
         elif choice == "7":
             ttype = input("Transaction Type : ")
             print(total_by_category(ttype))
             print()
+        #  Manage Categories
         elif choice == "8":
             print("1. Add Category")
             print("2. Delete Category")
             print("0. Back")
             ndchoice = input("Choose an option: ").strip()
+          # Add Category
             if ndchoice == "1":
                  category = input("Category : ")
                  ttype = input("income or expense? : ")
                  print(add_category(category, ttype))
                  print()
+          # Delete Category
             elif ndchoice == "2":
                 print(show_categories())
                 ttype = input("Select a type: ")
                 categoryy = input("Category: ")
                 print(delete_category(categoryy, ttype))
                 print()
+            # Back
             elif ndchoice == "0":
                 print()
             else:
                 print("❌ Invalid choice")
                 print()
-
+        # Delete Transaction
         elif choice == "9":
             print(delete_transaction())
             print()
+        # Exit
         elif choice == "0":
             return
         else:
